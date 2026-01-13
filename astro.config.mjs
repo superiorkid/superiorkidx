de742475c4cdd9d8b182c1c729ed9a4f5c28f704
superiorkid <mohammad.ilhamuddin@gmail.com>
2026-01-13 23:51:30 +0800

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
@@ -15,4 +17,8 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    includeFiles: ["./MOH_ILHAMUDDIN_CV_FULL_STACK.pdf"],
+  }),
 });
